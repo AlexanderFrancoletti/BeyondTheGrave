1f135e706c5e220bd11ac3998d1e1450c1079145
Alex Francoletti <alexander.francoletti@gmail.com>
2019-02-08 16:56:55 -0500

    Some unity things and a design doc

---
+++ BeyondTheGrave/Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player
+{
+    public float health;
+    public float meter;
+
+    //variable that contains all the information regarding what state the character is in
+    //includes: standing, crouching, airborne, stunned
+    public string charState;
+    public bool blocking;
+    public string name;
+    
+    public Player()
+    {
+        health = 10000f;
+        meter = 0f;
+        charState = "standing";
+        blocking = false;
+        name = "Player";
+    }
+
+    public Player(float h, string n)
+    {
+        health = h;
+        meter = 0f;
+        charState = "standing";
+        blocking = false;
+        name = n;
+    }
+}
