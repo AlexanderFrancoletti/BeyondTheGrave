d89c75b385db00a9c7c4a8cc071295d48cad8b40
Alex Francoletti <alexander.francoletti@gmail.com>
2019-02-16 03:19:30 -0500

    Beginning of player scripts

--- BeyondTheGrave/Assets/Scripts/Player.cs
+++ BeyondTheGrave/Assets/Scripts/Player.cs
@@ -6,7 +6,9 @@ public class Player
 {
     public float health;
     public float meter;
-
+    public float speed;
+    public Vector2 dash;
+    public float jumpSpeed;
     //variable that contains all the information regarding what state the character is in
     //includes: standing, crouching, airborne, stunned
     public string charState;
@@ -20,14 +22,20 @@ public class Player
         charState = "standing";
         blocking = false;
         name = "Player";
+        speed = 4.5f;
+        dash = new Vector2(speed*2, 0f);
+        jumpSpeed = 4f;
     }
 
-    public Player(float h, string n)
+    public Player(float h, string n, float s, Vector2 d, float f)
     {
         health = h;
         meter = 0f;
         charState = "standing";
         blocking = false;
         name = n;
+        speed = s;
+        dash = d;
+        jumpSpeed = f;
     }
 }
---
+++ BeyondTheGrave/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Player player;
+    private int jump;
+    private Vector2 directionalInput;
+    private float jumpInput;
+    private Vector2 Velocity;
+    private float friction;
+    private float halfHeight;
+    public bool grounded;
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        player = new Player();
+        jump = 1;
+        friction = player.speed*2;
+        halfHeight = transform.GetComponent<SpriteRenderer>().bounds.extents.y;
+        grounded = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - halfHeight - 0.04f), Vector2.down, 0.025f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        jumpInput = Input.GetAxis("Jump");
+        directionalInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if (grounded)
+        {
+            rb.AddForce(new Vector2((directionalInput.x * friction - rb.velocity.x) * friction, 0));
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y);
+            if (jumpInput > 0f)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, player.jumpSpeed * 6f);
+                grounded = false;
+            }
+        }
+        else
+        {
+            rb.AddForce(new Vector2((directionalInput.x/2 * friction - rb.velocity.x) * friction, 0));
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y);
+        }
+        grounded = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - halfHeight - 0.04f), Vector2.down, 0.025f);
+    }
+}
